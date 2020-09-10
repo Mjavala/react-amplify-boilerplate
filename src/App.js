beb6e97979646d7f5dcda0be5100e6cd3edbfebd
Jose Bravo <mjabravo221@gmail.com>
2020-09-09 17:57:14 -0700

    add withAuthenticator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify'
+import aws_exports from './aws-exports'
+import {withAuthenticator} from 'aws-amplify-react'
+Amplify.configure(aws_exports)
+
 
 function App() {
   return (
@@ -23,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App,true);
